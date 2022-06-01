2a1b695e2a00214ab6ce6f6d8184f4ba2d93da74
Brayan Rodallega <thebrayan1a1@outlook.com>
2022-06-01 04:06:20 -0500

    Proyecto integrado

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BrayanWeb.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Hi there 👋, this is my site 💙
